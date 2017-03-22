46d037a3dc7f0a652da08cf81d200b8f5d8cb697
David Jackson <davidjackson@MacBook-Pro.local>
2017-03-22 23:26:12 +0000

    Track story completeness
    Shows percentage of story in correct order

--- orderComparison.js
+++ orderComparison.js
@@ -1,7 +1,9 @@
+var turnsAllowed=6;
 var idsInOrder;
-var changes=0;
+var turns=0;
+var correctPos=0;
 var targetArray = ["part1", "part2", "part3", "part4", "part5", "part6", "part7"];
-
+var userArray;
 function commentOnChanges(){
 
 	if (changes<3){
@@ -19,43 +21,39 @@ function currentOrder(){
 //     if (userArray.length != targetArray.length)
 //         return false;
 // 
-//     for (var i = 0, l=userArray.length; i < l; i++) {
-//         // Check if we have nested arrays
-//         if (userArray[i] instanceof Array && targetArray[i] instanceof Array) {
-//             // recurse into the nested arrays
-//             if (!userArray[i].equals(targetArray[i]))
-//                 return false;       
-//         }           
-//         else if (userArray[i] != targetArray[i]) { 
-//             // Warning - two different object instances will never be equal: {x:20} != {x:20}
-//             return false;   
-//         }           
-//     }       
-//     return true;
+  	 correctPos=0;
+    for (var i = 0, l=userArray.length; i < l; i++) {
 
-A=targetArray.toString();
-B=userArray.toString();
-
-	if (A==B)
-		{
-
-		setTimeout(success, 100000);
-		var success = window.alert("You got the story straight in " + changes + " goes! " + commentOnChanges());
-		} 
-
-// 	else
-// 		{
-// 		setTimeout(failure, 100000);
-// 		var failure = window.alert("Not done!");
-// 
-// 		
-// 		}
+	 if (userArray[i] == targetArray[i]) { 
+            correctPos++;   
+            var currentLI=userArray[i];
+			var d = document.getElementById(currentLI);
+			d.className = "correct";        
+			}   
+		else {
+		var currentLI=userArray[i];
+			var d = document.getElementById(currentLI);
+			d.className = "default";        
+		}
+    }       
+	checkOrder();
 
 }
 
-function countChanges(){
-	changes++;
-	document.getElementById("chchchchanges").innerHTML=changes;
+function countTurns(){
+	turns++;
+	var turnsLeft=turnsAllowed-turns;
+	document.getElementById("chchchchanges").innerHTML=turnsLeft;
    }
+   
+function checkOrder(){
+	var allParts=userArray.length;
+	var correctPc=Math.round((100/allParts)*correctPos);
+	document.getElementById("correctPos").innerHTML=correctPc;
+	if (correctPc==100){
+		window.alert("You got the story straight in " + changes + " goes! " + commentOnChanges());
+  	 }
+   }
+$( "#sortable" ).on( "sortupdate", countTurns);
 $( "#sortable" ).on( "sortupdate", currentOrder);
-$( "#sortable" ).on( "sortupdate", countChanges);
+
